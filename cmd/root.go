6e3b13433d46ba79ffb80527cbcd2a89c6b82209
Navneet shukla <navneetshukla824@gmail.com>
2024-03-08 15:17:00 +0530

    Created a root command

---
+++ cmd/root.go
@@ -0,0 +1,8 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var RootCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create is CLI Tool for folder and file creation",
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/navneetshukl/create/cmd"
+
+func main() {
+	cmd.RootCmd.Execute()
+}
